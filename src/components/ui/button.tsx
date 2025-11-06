4dbae9d3c747c99e0076252a4ab2093e204de76d
VICTOR ANGEL ANTONIO GOMEZ <va21070541@bachilleresdesonora.edu.mx>
2025-11-06 03:11:11 +0000

    bien, me voy dando cuenta que al pasar el cursor por el boton para cambi

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-primary hover:text-primary-foreground hover:shadow-primary/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 hover:shadow-primary/50",
-        ghost: "hover:bg-accent hover:text-accent-foreground hover:shadow-primary/50",
+        ghost: "hover:bg-primary hover:text-primary-foreground hover:shadow-primary/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
