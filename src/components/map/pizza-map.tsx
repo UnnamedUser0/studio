73fcf2187efaba4c35b33fdc126b4c6daf1c95b2
VICTOR ANGEL ANTONIO GOMEZ <va21070541@bachilleresdesonora.edu.mx>
2025-11-08 01:32:53 +0000

    en el mapa, agrega los tipos de mapas estandar, satelite y relieve

--- src/components/map/pizza-map.tsx
+++ src/components/map/pizza-map.tsx
@@ -41,9 +41,25 @@ export default function PizzaMap({ pizzerias, onMarkerClick, selectedPizzeria }:
       const map = L.map(mapContainerRef.current).setView(HERMOSILLO_CENTER, 12);
       mapInstanceRef.current = map;
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      const standardLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+
+      const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+      });
+
+      const terrainLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      });
+
+      const baseMaps = {
+        "Estándar": standardLayer,
+        "Satélite": satelliteLayer,
+        "Relieve": terrainLayer,
+      };
+
+      L.control.layers(baseMaps).addTo(map);
     }
     
     // Cleanup function to remove the map instance
