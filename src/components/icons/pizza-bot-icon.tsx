a59b8ef7d37fe9e307f64bb350b12ab3f4558bd4
VICTOR ANGEL ANTONIO GOMEZ <va21070541@bachilleresdesonora.edu.mx>
2025-11-10 03:27:46 +0000

    quedo bien, pero en el saludo tomalo solamente de referencia de la image

--- src/components/icons/pizza-bot-icon.tsx
+++ src/components/icons/pizza-bot-icon.tsx
@@ -19,16 +19,16 @@ export function PizzaBotIcon(props: React.SVGProps<SVGSVGElement>) {
             {/* Crust */}
             <path d="M 45,32 C 55,18 75,10 100,10 C 125,10 145,18 155,32 L 45,32 Z" fill="#F4B459" stroke="#C78835" strokeWidth="4" strokeLinejoin="round" />
 
-            {/* Arm Left (Static) */}
-            <g id="pizzabot-arm-left">
+            {/* Arm Left (Waving) */}
+            <g id="pizzabot-arm" style={{ transformOrigin: '65px 95px' }}>
                 <path d="M 65,95 C 45,105 35,125 50,135" fill="none" stroke="black" strokeWidth="6" />
                 <path d="M40,145 C30,155 25,145 28,135 C35,120 50,125 50,135" fill="white" stroke="black" strokeWidth="2"/>
                 <path d="M 33,133 L 40,138" stroke="black" strokeWidth="1.5" strokeLinecap="round" />
                 <path d="M 36,128 L 43,133" stroke="black" strokeWidth="1.5" strokeLinecap="round" />
             </g>
             
-            {/* Arm Right (Waving) */}
-            <g id="pizzabot-arm" style={{ transformOrigin: '135px 95px' }}>
+            {/* Arm Right (Static) */}
+            <g id="pizzabot-arm-right">
                 <path d="M 135,95 C 155,85 170,95 165,115" fill="none" stroke="black" strokeWidth="6" />
                 <path d="M155,125 C150,135 165,140 175,130 C185,120 180,105 170,110" fill="white" stroke="black" strokeWidth="2"/>
                 <path d="M 164,115 L 170,122" stroke="black" strokeWidth="1.5" strokeLinecap="round" />
