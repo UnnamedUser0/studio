c10d4509eeb42daac510a409eed37e5ed1ffca3b
VICTOR ANGEL ANTONIO GOMEZ <va21070541@bachilleresdesonora.edu.mx>
2025-11-10 23:24:52 +0000

    siguen sin tener los efectos y recuerda que los botones del apartado de

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-primary/10 hover:text-primary",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "shadow-none hover:bg-accent hover:text-accent-foreground active:translate-y-0",
