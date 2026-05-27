92ae03d616685bffce2623b2995fecc8f07061fe
Angel <va21070541@bachilleresdesonora.edu.mx>
2026-05-27 14:05:55 -0700

    fix: configure trustHost for NextAuth to resolve UntrustedHost error behind proxy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,6 +10,7 @@ import bcrypt from "bcryptjs"
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     secret: process.env.AUTH_SECRET,
+    trustHost: true,
     providers: [
         Google({
             clientId: process.env.AUTH_GOOGLE_ID,
